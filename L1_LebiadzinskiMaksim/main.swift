11241bc3fb6e9069aa9cd94fe96cfac63434400b
MAKSIM LEBIADZINSKI <workmd5@yandex.ru>
2021-10-12 14:27:18 +0300

    Lesson 1

--- L1_LebiadzinskiMaksim/main.swift
+++ L1_LebiadzinskiMaksim/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+// Exercise 1
+// Solve the quadratic equation
+
+/*
+ Example: The quadratic equation
+ ax2 + bx + c = 0, where a ≠ 0
+ 
+ D = b2 − 4ac – сalculates the number of roots
+ */
+
+let a: Double = -10
+let b: Double = 15
+let c: Double = 100
+let x1: Double
+let x2: Double
+
+// Calculate discriminant
+let D = pow(b, 2) - 4 * a * c
+
+// Calculate equation
+
+if ( D < 0 ) {
+    print("Корней нет!")
+} else if (D == 0) {
+    x1 = ( -b + sqrt(D) ) / ( 2 * a )
+    print("Корень X = \(x1)")
+} else {
+    x1 = (-b + sqrt(D) ) / ( 2 * a )
+    x2 = (-b - sqrt(D) ) / ( 2 * a )
+    print("Корень X1 = \(x1)\nКорень X2 = \(x2)")
+}
 
