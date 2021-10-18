5450d9d8ce2b1020b911b7d702875c4c3de2926f
MAKSIM LEBIADZINSKI <workmd5@yandex.ru>
2021-10-18 14:28:00 +0300

    L2 HomeWork

--- L1_LebiadzinskiMaksim/main.swift
+++ L1_LebiadzinskiMaksim/main.swift
@@ -1,80 +1,44 @@
 //
 //  main.swift
-//  L1_LebiadzinskiMaksim
+//  L2_LebiadzinskiMaksim
 //
-//  Created by MAKSIM LEBIADZINSKI on 5.10.21.
+//  Created by MAKSIM LEBIADZINSKI on 18.10.21.
 //
 
 import Foundation
 
-// Exercise 1
-// Solve the quadratic equation
-
-print("Exercise 1\n")
-
-/*
- Example: The quadratic equation
- ax2 + bx + c = 0, where a ≠ 0
- 
- D = b2 − 4ac – сalculates the number of roots
- */
-
-let a: Double = -10
-let b: Double = 15
-let c: Double = 100
-let x1: Double
-let x2: Double
-
-// Calculate discriminant
-let D = pow(b, 2) - 4 * a * c
-
-// Calculate equation
-
-if ( D < 0 ) {
-    print("Корней нет!")
-} else if (D == 0) {
-    x1 = ( -b + sqrt(D) ) / ( 2 * a )
-    print("Корень X = \(x1)")
-} else {
-    x1 = (-b + sqrt(D) ) / ( 2 * a )
-    x2 = (-b - sqrt(D) ) / ( 2 * a )
-    print("Корень X1 = \(x1)\nКорень X2 = \(x2)")
+func isAnEven (_ inputNumber: Int) -> Bool {
+    return inputNumber % 2 == 0 ? true : false
 }
 
-print("\n============\n")
+func remainderOfDevision(_ inputNumber: Int) -> Bool {
+    return inputNumber % 3 == 0 ? true : false;
+}
 
-print("Exercise 2\n")
+var numIn = 102
 
-// Exercise 2
+if isAnEven(numIn) {
+    print("Число \(numIn) четное")
+} else {
+    print("Число \(numIn) нечетное")
+}
 
-let catOne: Double = 3 // Катет #1
-let catTwo: Double = 4 // Катет #2
-let S, P, H: Double
+numIn = 16
 
-S = 1/2 * (catOne * catTwo)
-P = sqrt(pow(catOne, 2) + pow(catTwo, 2)) + catOne + catTwo
-H = sqrt(pow(catOne, 2) + pow(catTwo, 2))
+if remainderOfDevision(numIn) {
+    print("\(numIn) делится на 3 без остатка")
+} else {
+    print("\(numIn) не делится на 3 без остатка")
+}
 
-print("Площадь = \(S)\nПериметр = \(P)\nГипотенуза = \(H)")
+var dataArray: [Int] = []
 
-print("\n============\n")
+for index in 101...200 {
+    dataArray.append(index)
+}
 
-print("Exercise 3\n")
+let filteredDataArray = dataArray.filter({
+    !isAnEven($0) && !remainderOfDevision($0)
+})
 
-// Exercise 3
-// Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
-
-let payIn: Decimal = 1000 // Сумма в USD
-let percentage: Decimal = 9 // Проценты в год;
-
-
-let mDays: Double;
-
-let payOut, percentOut: Decimal;
-
-// Рассчет простых процентов в конце срока
-
-percentOut = (payIn * percentage * 365 * 5 / 365) / 100
-payOut = payIn + percentOut
-
-print("По истечению 5 лет сумма на счету составит: \(payOut) USD\nПроценты за 5 лет составят: \(percentOut) USD");
+print(filteredDataArray)
